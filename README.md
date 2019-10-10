166bd48b971ee023e5a7ce3cf5c385ac630c57e0
Amit Dubey <amit.dubey@in-it1458.globant.com>
2019-10-10 14:24:40 +0530

    CR-Oct-19: Add boilerplate for angular assignment

---
+++ README.md
@@ -0,0 +1,15 @@
+Globant - Code Realm - Angular Assignment
+
+Please install all dependencies by running `npm i`.
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Running unit tests
+
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Running end-to-end tests
+
+Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, NoopAnimationsModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
